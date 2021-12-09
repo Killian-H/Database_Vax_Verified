f35fa2889b357e3e4d3dc09ad82fa3ee6ad9fe84
alex <jaffacake.zx@gmail.com>
2021-12-08 17:56:39 -0800

    no

---
+++ Database_Vax_Verified/Killian/config.php
@@ -0,0 +1,7 @@
+<?php
+define('DBHOST', 'localhost');
+define('DBNAME', 'business_covid');
+define('DBUSER', 'testuser');
+define('DBPASS', 'Naillik377!');
+define('DBCONNSTRING','mysql:dbname=business_covid;charset=utf8mb4');
+?>
